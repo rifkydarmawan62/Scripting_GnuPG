5c7c6626e9e329cfc71ab4e76052b18d4a899152
rifkydarmawan62 <rifkydarmawan62@gmail.com>
2024-01-27 18:17:24 +0700

    Optimalisasi Program

--- Download_GnuPG.py
+++ Download_GnuPG.py
@@ -1,8 +1,6 @@
 from subprocess import run, CalledProcessError
 from colorama import Fore, Back
 from tkinter.filedialog import askdirectory as pilih_direktori
-from os.path import exists
-from os import remove
 from platform import system
 
 if system().lower() == "windows":
@@ -14,24 +12,14 @@ if system().lower() == "windows":
     DIREKTORI_FOLDER = pilih_direktori(title = "Pilih lokasi unduhan file GnuPG disimpan").replace("/", "\\")
     if DIREKTORI_FOLDER:
         URL = "https://files.gpg4win.org/gpg4win-4.3.0.exe"
-        LOKASI_UNDUHAN = f"{DIREKTORI_FOLDER}\\gpg4win-4.3.0.exe"
-        PERINTAH = f"bitsadmin /transfer \"Mengunduh_GnuPG\" /download /priority FOREGROUND \"{URL}\" \"{LOKASI_UNDUHAN}\""
-        print(f"Menjalankan perintah Windows Command Prompt {Fore.BLACK}{Back.LIGHTYELLOW_EX}{PERINTAH}{Fore.RESET}{Back.RESET} ...")
+        LOKASI_UNDUHAN = f"{DIREKTORI_FOLDER}\\{URL.split("/")[-1]}"
+        PERINTAH = f"bitsadmin /transfer \"Mengunduh_GnuPG\" /download /priority FOREGROUND \"{URL}\" \"{LOKASI_UNDUHAN}\" && \"{LOKASI_UNDUHAN}\""
+        print(f"{Fore.YELLOW}Menjalankan perintah Windows Command Prompt {Fore.BLACK}{Back.LIGHTBLUE_EX}{PERINTAH}{Fore.YELLOW}{Back.RESET} ...{Fore.RESET}")
         try:
             run(PERINTAH, shell = True, check = True)
         except CalledProcessError:
-            print(f"{Fore.LIGHTRED_EX}Gagal mengunduh GnuPG!{Fore.RESET}")
-            if exists(LOKASI_UNDUHAN):
-                remove(LOKASI_UNDUHAN)
+            print(f"{Fore.LIGHTRED_EX}Gagal mengunduh atau menjalankan instalasi GnuPG!{Fore.RESET}")
         except KeyboardInterrupt:
-            print(f"{Fore.LIGHTRED_EX}Unduhan GnuPG dihentikan!{Fore.RESET}")
-            if exists(LOKASI_UNDUHAN):
-                remove(LOKASI_UNDUHAN)
+            print(f"{Fore.LIGHTRED_EX}Unduhan atau instalasi GnuPG dihentikan!{Fore.RESET}")
         else:
-            print(f"Menjalankan perintah instalasi GnuPG {Fore.BLACK}{Back.LIGHTYELLOW_EX}\"{LOKASI_UNDUHAN}\"{Fore.RESET}{Back.RESET} ...")
-            try:
-                run(f"\"{LOKASI_UNDUHAN}\"", shell = True, check = True)
-            except CalledProcessError:
-                print(f"{Fore.LIGHTRED_EX}Gagal menjalankan instalasi GnuPG!{Fore.RESET}")
-    else:
-        print(f"{Fore.LIGHTRED_EX}Unduhan instalasi GnuPG tidak disimpan!{Fore.RESET}")
+            print(f"{Fore.LIGHTGREEN_EX}Berhasil menjalankan instalasi GnuPG{Fore.RESET}")
